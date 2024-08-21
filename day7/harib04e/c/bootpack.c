6641b2a57ae0e90eb178cc3048660e6bd2551405
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-08-21 09:16:51 +0900

    copy harib04a/

---
+++ day7/harib04e/c/bootpack.c
@@ -0,0 +1,30 @@
+/*
+ * bootpack.c
+ */
+
+#include "bootpack.h"
+
+void HariMain() {
+  struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
+  char s[40];
+  unsigned char mcursor[256];
+
+  init_gdtidt();
+  init_pic();
+  io_sti();  // 割り込み許可
+
+  init_palette();
+  init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
+
+  int mx = (binfo->scrnx - 16) / 2;
+  int my = (binfo->scrny - 28 - 16) / 2;
+  init_mouse_cursor8(mcursor, COL8_008484);
+  putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+  my_sprintf(s, "(%d, %d)", mx, my);
+  putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+
+  io_out8(PIC0_IMR, 0xf9);  // PIC1&キーボード割り込み許可
+  io_out8(PIC1_IMR, 0xef);  // マウス割り込み許可
+
+  for (;;) io_hlt();
+}

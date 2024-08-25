b948e51fe41315ff19e1af007bb2659396c57218
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-08-25 19:19:31 +0900

    refactoring

--- day11/harib08b/c/fifo.c
+++ day11/harib08b/c/fifo.c
@@ -13,7 +13,6 @@ void fifo8_init(struct FIFO8 *fifo, int size, unsigned char *buf) {
   fifo->flags = 0;
   fifo->p = 0;  // Write index
   fifo->q = 0;  // Read index
-  return;
 }
 
 int fifo8_put(struct FIFO8 *fifo, unsigned char data) {
@@ -23,23 +22,16 @@ int fifo8_put(struct FIFO8 *fifo, unsigned char data) {
   }
   fifo->buf[fifo->p] = data;
   fifo->p++;
-  if (fifo->p == fifo->size) {
-    fifo->p = 0;
-  }
+  if (fifo->p == fifo->size) fifo->p = 0;
   fifo->free--;
   return 0;
 }
 
 int fifo8_get(struct FIFO8 *fifo) {
-  int data;
-  if (fifo->free == fifo->size) {
-    return -1;
-  }
-  data = fifo->buf[fifo->q];
+  if (fifo->free == fifo->size) return -1;
+  int data = fifo->buf[fifo->q];
   fifo->q++;
-  if (fifo->q == fifo->size) {
-    fifo->q = 0;
-  }
+  if (fifo->q == fifo->size) fifo->q = 0;
   fifo->free++;
   return data;
 }

269fa451a1ad2477eeaf3734577f6ff11887fbc6
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-09-19 12:43:06 +0900

    copy harib19d/

---
+++ day16-30/day22/harib19d/c/console.c
@@ -0,0 +1,298 @@
+/*
+ * console.c
+ */
+
+#include "bootpack.h"
+
+void console_task(struct SHEET *sheet, unsigned int memtotal) {
+  struct TASK *task = task_now();
+  int fifobuf[128];
+  fifo32_init(&task->fifo, 128, fifobuf, task);
+
+  struct TIMER *timer = timer_alloc();
+  timer_init(timer, &task->fifo, 1);
+  timer_settime(timer, 50);
+  struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
+  int *fat = (int *)memman_alloc_4k(memman, 4 * 2880);
+  file_readfat(fat, (unsigned char *)(ADR_DISKIMG + 0x000200));
+
+  // Display prompt
+  struct CONSOLE cons;
+  cons.sht = sheet;
+  cons.cur_x = 8;
+  cons.cur_y = 28;
+  cons.cur_c = -1;
+  *((int *)0x0fec) = (int)&cons;
+  cons_putchar(&cons, '>', 1);
+
+  char cmdline[30];
+  for (;;) {
+    io_cli();
+    if (fifo32_status(&task->fifo) == 0) {
+      task_sleep(task);
+      io_sti();
+    } else {
+      int data = fifo32_get(&task->fifo);
+      io_sti();
+      if (data <= 1) {  // Timer for cursor
+        if (data) {
+          timer_init(timer, &task->fifo, 0);
+          if (cons.cur_c >= 0) cons.cur_c = COL8_FFFFFF;
+        } else {
+          timer_init(timer, &task->fifo, 1);
+          if (cons.cur_c >= 0) cons.cur_c = COL8_000000;
+        }
+        timer_settime(timer, 50);
+      }
+      if (data == 2) cons.cur_c = COL8_FFFFFF;  // Cursor ON
+      if (data == 3) {                          // Cursor OFF
+        boxfill8(
+            sheet->buf,
+            sheet->bxsize,
+            COL8_000000,
+            cons.cur_x,
+            cons.cur_y,
+            cons.cur_x + 7,
+            cons.cur_y + 15
+        );
+        cons.cur_c = -1;
+      }
+      if (256 <= data && data <= 511) {  // Keyboard data from task_a
+        data -= 256;
+        if (data == 8) {  // Backspace
+          if (cons.cur_x > 16) {
+            cons_putchar(&cons, ' ', 0);
+            cons.cur_x -= 8;
+          }
+        } else if (data == 10) {  // Enter
+          // Remove cursor with space
+          cons_putchar(&cons, ' ', 0);
+          cmdline[cons.cur_x / 8 - 2] = 0;
+          cons_newline(&cons);
+          cons_runcmd(cmdline, &cons, fat, memtotal);
+          cons_putchar(&cons, '>', 1);
+        } else {  // Normal letter
+          if (cons.cur_x < 240) {
+            cmdline[cons.cur_x / 8 - 2] = data;
+            cons_putchar(&cons, data, 1);
+          }
+        }
+      }
+      // Show cursor again
+      if (cons.cur_c >= 0)
+        boxfill8(
+            sheet->buf,
+            sheet->bxsize,
+            cons.cur_c,
+            cons.cur_x,
+            cons.cur_y,
+            cons.cur_x + 7,
+            cons.cur_y + 15
+        );
+
+      sheet_refresh(sheet, cons.cur_x, cons.cur_y, cons.cur_x + 8, cons.cur_y + 16);
+    }
+  }
+}
+
+void cons_putchar(struct CONSOLE *cons, int chr, char move) {
+  char s[2];
+  s[0] = chr;
+  s[1] = 0;
+  if (s[0] == 0x09) {  // Tab
+    for (;;) {
+      putfonts8_asc_sht(cons->sht, cons->cur_x, cons->cur_y, COL8_FFFFFF, COL8_000000, " ", 1);
+      cons->cur_x += 8;
+      if (cons->cur_x == 8 + 240) {
+        cons_newline(cons);
+      }
+      if (((cons->cur_x - 8) & 0x1f) == 0) break;
+    }
+  } else if (s[0] == 0x0a) {  // New-line
+    cons_newline(cons);
+  } else if (s[0] == 0x0d) {
+  } else {
+    putfonts8_asc_sht(cons->sht, cons->cur_x, cons->cur_y, COL8_FFFFFF, COL8_000000, s, 1);
+    if (move != 0) {
+      cons->cur_x += 8;
+      if (cons->cur_x == 8 + 240) cons_newline(cons);
+    }
+  }
+}
+
+void cons_newline(struct CONSOLE *cons) {
+  struct SHEET *sheet = cons->sht;
+  if (cons->cur_y < 28 + 112) {
+    cons->cur_y += 16;  // Next line
+  } else {              // Scroll
+    for (int y = 28; y < 28 + 112; y++) {
+      for (int x = 8; x < 8 + 240; x++)
+        sheet->buf[x + y * sheet->bxsize] = sheet->buf[x + (y + 16) * sheet->bxsize];
+    }
+    for (int y = 28 + 112; y < 28 + 128; y++) {
+      for (int x = 8; x < 8 + 240; x++) sheet->buf[x + y * sheet->bxsize] = COL8_000000;
+    }
+    sheet_refresh(sheet, 8, 28, 8 + 240, 28 + 128);
+  }
+  cons->cur_x = 8;
+}
+
+void cons_putstr0(struct CONSOLE *cons, char *s) {
+  for (; *s != 0; s++) cons_putchar(cons, *s, 1);
+}
+
+void cons_putstr1(struct CONSOLE *cons, char *s, int l) {
+  for (int i = 0; i < l; i++) cons_putchar(cons, s[i], 1);
+}
+
+void cons_runcmd(char *cmdline, struct CONSOLE *cons, int *fat, unsigned int memtotal) {
+  if (my_strcmp(cmdline, "mem") == 0) {
+    cmd_mem(cons, memtotal);
+  } else if (my_strcmp(cmdline, "cls") == 0) {
+    cmd_cls(cons);
+  } else if (my_strcmp(cmdline, "dir") == 0) {
+    cmd_dir(cons);
+  } else if (my_strncmp(cmdline, "type ", 5) == 0) {
+    cmd_type(cons, fat, cmdline);
+  } else if (cmdline[0] != 0) {
+    if (cmd_app(cons, fat, cmdline) == 0) cons_putstr0(cons, "Bad command.\n\n");
+  }
+}
+
+void cmd_mem(struct CONSOLE *cons, unsigned int memtotal) {
+  struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
+  char s[60];
+  my_sprintf(
+      s, "total   %dMB\nfree %dKB\n\n", memtotal / (1024 * 1024), memman_total(memman) / 1024
+  );
+  cons_putstr0(cons, s);
+}
+
+void cmd_cls(struct CONSOLE *cons) {
+  struct SHEET *sheet = cons->sht;
+  for (int y = 28; y < 28 + 128; y++) {
+    for (int x = 8; x < 8 + 240; x++) sheet->buf[x + y * sheet->bxsize] = COL8_000000;
+  }
+  sheet_refresh(sheet, 8, 28, 8 + 240, 28 + 128);
+  cons->cur_y = 28;
+}
+
+void cmd_dir(struct CONSOLE *cons) {
+  struct FILEINFO *finfo = (struct FILEINFO *)(ADR_DISKIMG + 0x002600);
+  char s[30];
+  for (int i = 0; i < 224; i++) {
+    if (finfo[i].name[0] == 0x00) break;
+    if (finfo[i].name[0] != 0xe5) {
+      if ((finfo[i].type & 0x18) == 0) {
+        my_sprintf(s, "filename.ext   %d\n", finfo[i].size);
+        for (int j = 0; j < 8; j++) s[j] = finfo[i].name[j];
+        s[9] = finfo[i].ext[0];
+        s[10] = finfo[i].ext[1];
+        s[11] = finfo[i].ext[2];
+        cons_putstr0(cons, s);
+      }
+    }
+  }
+  cons_newline(cons);
+}
+
+void cmd_type(struct CONSOLE *cons, int *fat, char *cmdline) {
+  struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
+  struct FILEINFO *finfo =
+      file_search(cmdline + 5, (struct FILEINFO *)(ADR_DISKIMG + 0x002600), 224);
+
+  if (finfo != 0) {  // Found the file
+    char *p = (char *)memman_alloc_4k(memman, finfo->size);
+    file_loadfile(finfo->clustno, finfo->size, p, fat, (char *)(ADR_DISKIMG + 0x003e00));
+    cons_putstr1(cons, p, finfo->size);
+    memman_free_4k(memman, (int)p, finfo->size);
+  } else {  // Didn't find the file
+    cons_putstr0(cons, "File not found.\n");
+  }
+  cons_newline(cons);
+}
+
+int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline) {
+  struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
+  struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *)ADR_GDT;
+  struct TASK *task = task_now();
+
+  char name[18];
+  int i;
+  // Make filename from command
+  for (i = 0; i < 13; i++) {
+    if (cmdline[i] <= ' ') break;
+    name[i] = cmdline[i];
+  }
+  name[i] = 0;
+
+  // Look for file
+  struct FILEINFO *finfo = file_search(name, (struct FILEINFO *)(ADR_DISKIMG + 0x002600), 224);
+  // Not found, add ".HRB" and search again
+  if (finfo == 0 && name[i - 1] != '.') {
+    name[i] = '.';
+    name[i + 1] = 'H';
+    name[i + 2] = 'R';
+    name[i + 3] = 'B';
+    name[i + 4] = 0;
+    finfo = file_search(name, (struct FILEINFO *)(ADR_DISKIMG + 0x002600), 224);
+  }
+
+  if (finfo != 0) {  // Found the file
+    char *p = (char *)memman_alloc_4k(memman, finfo->size);
+    char *q = (char *)memman_alloc_4k(memman, 64 * 1024);
+    *((int *)0xfe8) = (int)p;
+    file_loadfile(finfo->clustno, finfo->size, p, fat, (char *)(ADR_DISKIMG + 0x003e00));
+    set_segmdesc(gdt + 1003, finfo->size - 1, (int)p, AR_CODE32_ER + 0x60);
+    set_segmdesc(gdt + 1004, 64 * 1024 - 1, (int)q, AR_DATA32_RW + 0x60);
+    if (finfo->size >= 8 && my_strncmp(p + 4, "Hari", 4) == 0) {
+      p[0] = 0xe8;
+      p[1] = 0x16;
+      p[2] = 0x00;
+      p[3] = 0x00;
+      p[4] = 0x00;
+      p[5] = 0xcb;
+    }
+    start_app(0, 1003 * 8, 64 * 1024, 1004 * 8, &(task->tss.esp0));
+    memman_free_4k(memman, (int)p, finfo->size);
+    memman_free_4k(memman, (int)q, 64 * 1024);
+    cons_newline(cons);
+    return 1;
+  }
+
+  // Not found
+  return 0;
+}
+
+int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax) {
+  int cs_base = *((int *)0xfe8);
+  struct TASK *task = task_now();
+  struct CONSOLE *cons = (struct CONSOLE *)*((int *)0x0fec);
+  if (edx == 1) {
+    cons_putchar(cons, eax & 0xff, 1);
+  } else if (edx == 2) {
+    cons_putstr0(cons, (char *)ebx + cs_base);
+  } else if (edx == 3) {
+    cons_putstr1(cons, (char *)ebx + cs_base, ecx);
+  } else if (edx == 4) {
+    return &task->tss.esp0;
+  }
+  return 0;
+}
+
+int *inthandler0c(int *esp) {
+  struct CONSOLE *cons = (struct CONSOLE *)*((int *)0x0fec);
+  struct TASK *task = task_now();
+  char s[30];
+  cons_putstr0(cons, "\nINT 0C :\n Stack Exception.\n");
+  my_sprintf(s, "EIP = %X\n", esp[11]);
+  cons_putstr0(cons, s);
+  return &task->tss.esp0;  // Exception halt
+}
+
+int *inthandler0d(int *esp) {
+  struct CONSOLE *cons = (struct CONSOLE *)*((int *)0x0fec);
+  struct TASK *task = task_now();
+  cons_putstr0(cons, "\nINT 0D :\n General Protected Exception.\n");
+  return &(task->tss.esp0);
+}

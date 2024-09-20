c99f5297fdcc4d6d358eb1e97089ea01eed9b934
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-09-20 12:40:07 +0900

    update harib24a/

--- day16-30/day27/harib24a/c/bootpack.c
+++ day16-30/day27/harib24a/c/bootpack.c
@@ -185,6 +185,7 @@ void HariMain() {
             task->tss.eax = (int)&(task->tss.esp0);
             task->tss.eip = (int)asm_end_app;
             io_sti();
+            task_run(task, -1, 0);
           }
         }
         if (data == 0x3c && key_shift != 0) {  // Shift + F2
@@ -257,6 +258,7 @@ void HariMain() {
                         task->tss.eax = (int)&(task->tss.esp0);
                         task->tss.eip = (int)asm_end_app;
                         io_sti();
+                        task_run(task, -1, 0);
                       } else {  // Console
                         struct TASK *task = sht->task;
                         io_cli();

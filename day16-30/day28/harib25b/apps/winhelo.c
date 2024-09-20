7c838e2b6eddbf38a7948d96a997d2287894b92a
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-09-20 18:54:26 +0900

    update harib25b/

--- day16-30/day28/harib25b/apps/winhelo.c
+++ day16-30/day28/harib25b/apps/winhelo.c
@@ -1,7 +1,7 @@
 #include "apilib.h"
-char buf[150 * 50];
 
 void HariMain() {
+  char buf[150 * 50];
   int win = api_openwin(buf, 150, 50, -1, "hello");
   for (;;) {
     if (api_getkey(1) == 0x0a) break;
--- day16-30/day28/harib25b/apps/winhelo2.c
+++ day16-30/day28/harib25b/apps/winhelo2.c
@@ -1,7 +1,7 @@
 #include "apilib.h"
-char buf[150 * 50];
 
 void HariMain() {
+  char buf[150 * 50];
   int win = api_openwin(buf, 150, 50, -1, "hello");
   api_boxfilwin(win, 8, 36, 141, 43, 3);
   api_putstrwin(win, 28, 28, 0, 12, "hello, world");

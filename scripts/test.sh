1abb5548151a5e926d20c7897d6c74a6276ae6a9
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-09-20 17:22:49 +0900

    improve test

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,9 +4,11 @@ cd ./scripts/..
 
 for f in ./*/*/*/Makefile; do
     dir=$(dirname $f)
-    echo "running $dir..."
-    cd $dir &&
-        make run &
+    echo "running $dir ..."
+    if [ $(cd $dir && make exists) = "true" ]; then
+        continue
+    fi
+    cd $dir && make run &
     sleep 2
     qemu_pid=$(ps -e -o pid,cmd | grep qemu | grep -v grep | awk '{ print $1 }')
     kill $qemu_pid

1f8b4fa5be8c4beda2b95075523bcf507680370a
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-09-21 08:58:07 +0900

    copy harib26a/

---
+++ day16-30/day29/harib26a/apps/sosu.c
@@ -0,0 +1,16 @@
+#include "apilib.h"
+
+#define MAX 1000
+
+void HariMain() {
+  char flag[MAX], s[8];
+  for (int i = 0; i < MAX; i++) flag[i] = 0;
+  for (int i = 2; i < MAX; i++) {
+    if (flag[i] == 0) {
+      my_sprintf(s, "%d ", i);
+      api_putstr0(s);
+      for (int j = i * 2; j < MAX; j += i) flag[j] = 1;
+    }
+  }
+  api_end();
+}
